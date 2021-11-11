9742c7ba96e6ea8b5e9e0ea6606db1a3d380f961
Yengin Loay <yengin.loay@gmail.com>
2021-11-10 23:13:39 -0600

    onclick logic linked to playtime

--- js/chart-selector.js
+++ js/chart-selector.js
@@ -1,8 +1,19 @@
-// This script shows and hides all of the different charts
-// No longer needed since using BS classes but keeping it here just in case
+// show and hides all of the different charts
+
+// import { month } from "./playtime.js";
 
 
-// function hidePlaytimeAll() {
-//     let chart = document.getElementById('playtime-all');
-//     chart.style.display = chart.style.display === "none" ? "block" : "none";
-// }
+// exporting stops chart from showing
+
+function showPlaytime() {
+    let chart = document.getElementById('playtime-month');
+    chart.style.display = "block";
+    // chart.style.display = chart.style.display === "none" ? "block" : "none";
+}
+
+function yearPlaytime() {
+
+}
+
+
+
