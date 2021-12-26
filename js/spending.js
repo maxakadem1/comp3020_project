c089f8d86ce036a2cc38f479412357be3846c86f
Yengin Loay <yengin.loay@gmail.com>
2021-12-25 19:06:15 -0600

    added legend to global chart

--- js/spending.js
+++ js/spending.js
@@ -3,12 +3,13 @@ const defaultLabel = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '1
 const title = 'Spending History'
 const xLabel = 'Week'
 const yLabel = 'Dollars Spent (CAD)'
+const spendingData1 = 'Total Spending'
 
 // <block:setup:1>
 const data = {
     labels: defaultLabel,
     datasets: [{
-        label: '',
+        label: spendingData1,
         backgroundColor: 'rgb(255, 99, 132, 0.7)',
         borderColor: 'rgb(255, 99, 132)',
         data: JSON.parse(localStorage.getItem('spendingData'))
@@ -27,7 +28,7 @@ const configSpending = {
                 text: title
             },
             legend: {
-                display: false
+                display: true,
             }
         },
         scales: {
